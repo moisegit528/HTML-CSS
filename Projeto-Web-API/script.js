4f88477fe62264755ec9801627daa75215028db0
moisegit528 <moisesgithub@gmail.com>
2026-05-24 21:28:00 -0300

    Atualizando página cadastro

--- Projeto-Web-API/script.js
+++ Projeto-Web-API/script.js
@@ -1,41 +1,18 @@
-const formulario = document.querySelector("form");
-const Inome = document.querySelector("#nome");
-const Iemail = document.querySelector("#email");
-const Isenha = document.querySelector("#senha");
-const Inascimento = document.querySelector("#nascimento");
-const Itelefone = document.querySelector("#telefone");
+document.getElementById("formCadastro").addEventListener("submit", async function(e) {
+    e.preventDefault();
 
-function cadastro(){
-    fetch("http://localhost:8080/cadastro",
-        {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            method: "POST",
-            body: JSON.stringify({
-                nome: Inome.value,
-                email: Iemail.value,
-                senha: Isenha.value,
-                nascimento: Inascimento.value,
-                telefone: Itelefone.value
-            })
-        })
-        .then(function (res) {console.log(res) })
-        .catch(function (res) {console.log(res) })
-};
-
-function limpar(){
-    Inome.value = "";
-    Iemail.value = "";
-    Isenha.value = "";
-    Inascimento.value = "";
-    Itelefone.value = "";
-};
-
-formulario.addEventListener('submit', function(event){
-    event.preventDefault();
-    
-    cadastro();
-    limpar();
-});
+    const usuario = {
+        nome: document.getElementById("nome").value,
+        email: document.getElementById("email").value,
+        senha: document.getElementById("senha").value,
+        telefone: document.getElementById("telefone").value
+    };
+    const response = await fetch("http://localhost:8080/usuarios",{
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(usuario)
+    });
+    const data = await response.json();
+    console.log("Usuário cadastrado: ", data);
+    alert("Usuário cadastro com sucesso!");
+})
